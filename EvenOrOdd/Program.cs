6202303d9b6dfee14770f7ba383efd5024f73e10
RokerDev <rokerdev91@gmail.com>
2022-07-02 21:38:35 +0200

    add program logic

--- EvenOrOdd/Program.cs
+++ EvenOrOdd/Program.cs
@@ -10,6 +10,38 @@ namespace EvenOrOdd
     {
         static void Main(string[] args)
         {
+            var play = true;
+            while (play)
+            {
+                Console.WriteLine("Please give me the number and I'll tell you if it's even or not:");
+                if (!long.TryParse(Console.ReadLine(), out long num))
+                {
+                    Console.WriteLine("You have to give me a number. Please try again.");
+                    continue;
+                }
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("The number you gave to me is Even.");
+                }
+                else if (num % 2 == 1)
+                {
+                    Console.WriteLine("The number you gave to me is Odd.");
+                }
+
+                Console.WriteLine("Do you wanna check another number? Yes or No:");
+                var userInput = Console.ReadLine();
+                var list = new List<string>() { "Y", "y", "yes", "Yes" };
+                if (list.Contains(userInput))
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else
+                {
+                    play = false;
+                }
+
+            }
         }
     }
 }
